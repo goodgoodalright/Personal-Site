be33ebb6db4b55a0993656e1563e55a33cca9fce
Daniel Soto <danny.desoto13@gmail.com>
2023-02-11 12:22:57 -0700

    set up reviews carousel, needs fixing

--- project folders/restaurant-jf/restaurant.js
+++ project folders/restaurant-jf/restaurant.js
@@ -13,12 +13,29 @@ function scroll() {
 // carousel
 let slideIndex = 1;
 showSlides(slideIndex);
-function changeSlide(n) {showSlides(slideIndex += n);}
-function currentSlide(n) {showSlides(slideIndex = n);}
-function showSlides(n) {
+function changeMenuSlide(n) {showMenuSlides(slideIndex += n);}
+function changeReviewSlide(n) {showReviewSlides(slideIndex += n);}
+function currentMenuSlide(n) {showMenuSlides(slideIndex = n);}
+function currentReviewSlide(n) {showReviewSlides(slideIndex = n);}
+function showMenuSlides(n) {
      let i;
-     let slides = document.getElementsByClassName("slide-wrap");
-     let dots = document.getElementsByClassName("dot-wrap");
+     let slides = document.getElementsByClassName("slide-wrap menu");
+     let dots = document.getElementsByClassName("dot-wrap menu");
+     if (n > slides.length) {slideIndex = 1}
+     if (n < 1) {slideIndex = slides.length}
+     for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = 'none';
+     }
+     for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace("active", "");
+     }
+     slides[slideIndex-1].style.display = "block";
+     dots[slideIndex-1].className += "active";
+}
+function showReviewSlides(n) {
+     let i;
+     let slides = document.getElementsByClassName("slide-wrap review");
+     let dots = document.getElementsByClassName("dot-wrap review");
      if (n > slides.length) {slideIndex = 1}
      if (n < 1) {slideIndex = slides.length}
      for (i = 0; i < slides.length; i++) {
